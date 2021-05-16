2f781d9fd82cd6e506f0b0bcf9fa39db8ea44bba
Rampagy <alexjsimon32@gmail.com>
2021-05-16 13:40:53 -0500

    Adding initial position class

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,7 +1,16 @@
-// Your First Program
+import position.PositionClass;
 
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Main
+{
+    public static void main(String[] args)
+    {
+        PositionClass point = new PositionClass(1, 2);
+        System.out.println(String.valueOf(point.x) + ":" + String.valueOf(point.y));
+
+
+        /*for (Position p : point.get_surrounding_positions())
+        {
+            System.out.println(String.valueOf(p.x) + ":" + String.valueOf(p.y));
+        }*/
     }
 }
