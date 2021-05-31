df8b0c656cd8959264b78d2cf2c55a636a9fdee5
Rampagy <alexjsimon32@gmail.com>
2021-05-30 21:08:33 -0500

    Initial portion of C# algorithm

--- C#/src/astar.cs
+++ C#/src/astar.cs
@@ -1,11 +1,49 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
-
-public partial class astar
+namespace astar
 {
-    public static void Main(string[] args)
+    public partial class main
     {
-        Console.WriteLine("Hello World!");
-        //HelloAgain();
+        static Random rnd = new Random();
+        const int MAP_WIDTH = 500;
+        const int MAP_HEIGHT = 500;
+        const int SEARCH_ITERATIONS = 100;
+        static List<List<int>> maze = new List<List<int>>();
+        static double total_time = 0;
+
+
+        public static void Main(string[] args)
+        {
+            Position start = new Position(0, 0);
+            Position goal = new Position(MAP_WIDTH - 1, MAP_HEIGHT - 1);
+            List<Position> path = new List<Position>();
+
+            // generate the maze
+            generateMaze();
+
+            // make sure the start and end are walkable
+            maze[start.y][start.x] = 0;
+            maze[goal.y][goal.x] = 0;
+
+            Console.WriteLine(maze.Count);
+            Console.WriteLine(maze[0].Count);
+        }
+
+        static void generateMaze()
+        {
+            // height
+            for (int i = 0; i < MAP_HEIGHT; i++)
+            {
+                List<int> maze_row = new List<int>();
+                // width
+                for (int j = 0; j < MAP_WIDTH; j++)
+                {
+                    maze_row.Add(rnd.Next(0, 255));
+                }
+                maze.Add(maze_row);
+            }
+        }
     }
 }
--- C#/src/position.cs
+++ C#/src/position.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
-public partial class astar
+
+namespace astar
 {
-    private class Position
+    /// <summary>
+    /// A position is an object with x and y values indicating the absolute position on the map.
+    /// </summary>
+    public class Position
     {
+        public readonly int x;
+        public readonly int y;
 
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Returns a list of all positions around the given position in each cardinal direction
+        /// </summary>
+        public List<Position> GetSurroundingCardinals()
+        {
+            List<Position> neighbors = new List<Position>();
+            neighbors.Add(new Position(this.x + 0, this.y - 1)); // north
+            neighbors.Add(new Position(this.x + 1, this.y + 0)); // east
+            neighbors.Add(new Position(this.x + 0, this.y + 1)); // south
+            neighbors.Add(new Position(this.x - 1, this.y + 0)); // west
+            return neighbors;
+        }
+
+        public override bool Equals(object o)
+        {
+            if (this == o)return true;
+            if (o == null || this.GetType() != o.GetType()) return false;
+
+            Position position = (Position) o;
+
+            if (x != position.x) return false;
+            return y== position.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.x >= this.y) ? this.x * this.x + this.x + this.y : this.x + this.y * this.y; // szudziks function
+        }
     }
 }
